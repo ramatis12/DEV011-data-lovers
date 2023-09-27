c85e4e66bfa45d8348402c35f88e3f55065b2784
sylpoio <syl.gomez.bio@gmail.com>
2023-09-26 20:22:42 -0600

    click moreInfo working fine

--- src/view.js
+++ src/view.js
@@ -6,8 +6,7 @@ export const renderItems = (data) => {
 
   const ul = document.createElement("ul");
   ul.id = "container";
-
-
+//console.log(data);
   //identificacmos los datos de las peliculas
   films.forEach((movie) => {
     titlesHTML += `
@@ -32,30 +31,19 @@ export const renderItems = (data) => {
   return ul;
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  const masInfo = document.querySelectorAll(".info");
-  let arrInfo = [];
-  masInfo.forEach((eventClick) => {
-    eventClick.addEventListener("click", (e) => {
-      data2.films.filter((movie) => {
-        if (movie.id === e.target.id) {
-          arrInfo = movie;
-        }
-      });
-      localStorage.setItem("movieInfoLocalStorage", JSON.stringify(arrInfo));
-      window.location.href = "info.html";
-    });
-  });
-});
+
 
 export const renderItems2 = () => {
-  const info = JSON.parse(localStorage.getItem("movieInfoLocalStorage"));  //identificacmos los datos de las peliculas
+  const info = JSON.parse(localStorage.getItem("movieInfoLocalStorage"));
+  //console.log(info);
+  //identificacmos los datos de las peliculas
   let peopleHTML = "";
   const arrPeople = info.people;
 
   const ul2 = document.createElement("ul");
   ul2.id = "container";
 
+  //console.log(arrPeople);
   arrPeople.forEach((people) => {
     peopleHTML += `
      <li class="card">
@@ -76,6 +64,7 @@ export const renderItems2 = () => {
         </table>
      </li> `;
   });
+  //console.log(tHTML);
   ul2.innerHTML = peopleHTML;
   return ul2;
 };
@@ -84,6 +73,7 @@ export const renderItems2 = () => {
 
 export const createOptions = (data, option) => {
   const optionsMap = data.films.map((elements) => {
+   // console.log(elements[option]);
     return elements[option];
   });
   const uniqueOptions = optionsMap.filter((item, index) => {
@@ -94,5 +84,6 @@ export const createOptions = (data, option) => {
   uniqueOptions.forEach((element) => {
     options += `<option value="${element}">${element}</option>`;
   });
+  //console.log(options);
   return options;
 };
